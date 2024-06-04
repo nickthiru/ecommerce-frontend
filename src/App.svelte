44d5da54a22a8018bf366fd51e005605f48d5e61
NICKT-LAPTOP\nickt <nickthiru@gmail.com>
2024-06-04 12:36:02 +0530

    Starting buiding up project from earlier projects, namely the Movie Review app and the Command and Control Center app.

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,23 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import {Route, Router, Link} from "svelte-routing";
+  import "@picocss/pico";
+
+  import NavBar from "./lib/common/NavBar.svelte";
+  import HomePage from "./lib/page/HomePage.svelte";
+  import SignUpPage from "./lib/page/SignUpPage.svelte";
+  import SignInPage from "./lib/page/SignInPage.svelte";
+  import VerifyOtpPage from "./lib/page/VerifyOtpPage.svelte";
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+<Router>
+  <NavBar />
 
-  <div class="card">
-    <Counter />
-  </div>
+  <main class="container">
+    <Route path={"/"} component={HomePage} />
+    <Route path={"/sign-up"} component={SignUpPage} />  
+    <Route path={"/verify-otp"} component={VerifyOtpPage} />
+    <Route path={"/sign-in"} component={SignInPage} />
+  </main>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
+</Router>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
---
+++ src/lib/common/NavBar.svelte
@@ -0,0 +1,14 @@
+<nav>
+  <ul>
+    <li><strong>Logo</strong></li>
+  </ul>
+  <ul>
+    <li><a href="/">About</a></li>
+    <li><a href="/">Services</a></li>
+    <li><a href="/">Products</a></li>
+  </ul>
+  <ul>
+    <li><a href="/">Sign In</a></li>
+    <li><a href="/">Sign Up</a></li>
+  </ul>
+</nav>
